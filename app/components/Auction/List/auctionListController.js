40b3a5dacec379545a20e0e5ca59439669973d2c
merynek <meravy.martin@gmail.com>
2016-11-02 22:09:04 +0100

    22:09 - 2.11

--- app/components/Auction/List/auctionListController.js
+++ app/components/Auction/List/auctionListController.js
@@ -8,8 +8,14 @@
 angular.module('appControllers')
   .controller('auctionListController', ['$scope', 'AuctionList', function($scope, AuctionList){
     $scope.AuctionList = AuctionList;
+    middle_no_padding();
+    $(window).resize(function () {
+      if(window.innerWidth <= 900)
+      {
+        middle_no_padding();
+      }
+    });
   }
-
 ]);
 
 
--- js/loader.js
+++ js/loader.js
@@ -5,6 +5,9 @@ function startLoading(text) {
 function endLoading(text) {
 	$(".loadPage").css("display", "none");
 }
+function middle_no_padding(){
+	$(".middle").addClass("no-padding");
+}
 //1->success(green) 2->warning(orange) 3->error(red)
 function message(type, message){
 	if(!message_active) {
