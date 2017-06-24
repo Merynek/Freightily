0a7981b2f06116587c9ea833192b79bd222c3b6d
Aleš Dopita <aloushdopita@gmail.com>
2017-06-24 18:41:47 +0200

    Detail shipment photos.

--- js/routeMap.js
+++ js/routeMap.js
@@ -46,14 +46,16 @@ function showRouteMap(route, id_auction){
 
                 var _SELF = this;
                 var distance = document.getElementById('distance');
-                this.directionsHelper_.route(request, function(response, status) {
-                    if (status == google.maps.DirectionsStatus.OK) {
-                        _SELF.directionsDisplay_.setDirections(response);
-                        distance.innerHTML = response.routes[0].legs[0].distance.text;
-                        return;
-                    }
-                    console.log('Directions Status: ' + status);
-                });
+                if (distance) {
+                    this.directionsHelper_.route(request, function(response, status) {
+                        if (status == google.maps.DirectionsStatus.OK) {
+                            _SELF.directionsDisplay_.setDirections(response);
+                            distance.innerHTML = response.routes[0].legs[0].distance.text;
+                            return;
+                        }
+                        console.log('Directions Status: ' + status);
+                    });
+                }
             },
 
             init: function(mapid) {
