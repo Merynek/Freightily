0478e32ffc6453cbda92890d56cba1db547333ff
Martin Meravý <m.meravy@gmc.net>
2019-01-03 16:46:45 +0100

    - minification files

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,56 @@ gulp.task('server', function() {
 	fallback: 'index.html'
 	});
 });
+
+const closureCompiler = require('google-closure-compiler').gulp();
+
+gulp.task('js-compile', function () {
+    return gulp.src([
+        'js/i18next-1.10.1.min.js',
+        'js/ng-i18next.js',
+        'app/config.js',
+        'js/shared.js',
+        'js/loader.js',
+        'js/map.js',
+        'app/app.js',
+        'app/shared/Services/ShipmentsService.js',
+        'app/shared/Services/AuctionService.js',
+        'app/shared/Services/UserService.js',
+        'app/shared/Services/UserAbilityService.js',
+        'app/shared/Services/AdminService.js',
+        'app/shared/mainController.js',
+        'app/components/Account/Login/loginController.js',
+        'app/components/Account/Registration/registrationController.js',
+        'app/components/Account/Profile/userProfileController.js',
+        'app/components/Account/ForgotPassword/forgotPasswordController.js',
+        'app/components/Auction/Create/createAuctionController.js',
+        'app/components/Auction/List/auctionListController.js',
+        'app/components/Auction/CreatedAuction/createdAuctionController.js',
+        'app/components/Auction/FavouriteAuction/favouriteAuctionController.js',
+        'app/components/Auction/WinAuction/winAuctionController.js',
+        'app/components/Auction/BidsAuction/bidsAuctionController.js',
+        'app/components/Shipments/new/newShipmentsController.js',
+        'app/components/Shipments/finished/finishedShipmentsController.js',
+        'app/components/Company/companyController.js',
+        'app/components/Company/AddDriver/addDriverController.js',
+        'app/components/Admin/Users/usersController.js',
+        'app/components/Admin/Map/mapController.js',
+        'app/shared/directives/DetailAuction/detailAuctionDirective.js',
+        'app/shared/directives/DetailShipment/detailShipmentDirective.js',
+        'app/shared/directives/DetailUser/detailUserDirective.js',
+        'app/shared/directives/Menu/menuDirective.js',
+        'app/shared/directives/egFiles.js',
+        'app/shared/directives/pagingDirective.js',
+        'app/routes.js'
+    ], {base: './'})
+        .pipe(closureCompiler({
+            compilation_level: 'SIMPLE',
+            warning_level: 'VERBOSE',
+            jscomp_off: ['checkVars', 'suspiciousCode'],
+            output_wrapper: '(function(){\n%output%\n}).call(this)',
+            js_output_file: 'output.min.js'
+        }, {
+            platform: ['native', 'java', 'javascript']
+        }))
+        .pipe(gulp.dest('./dist/js'));
+});
