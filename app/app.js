d7d09fce8de778ca9e88afa9c14999cdc8b49526
m.meravy <m.meravy@quadient.com>
2019-02-08 11:12:55 +0100

    - tooltipy

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@
         angular.module('appDirectives', []);
 
         angular.module('myApp', [
-                'appServices', 'appControllers', 'appDirectives', 'ui.router', 'ui.router.state.events', 'ngDialog', 'restangular', 'jm.i18next', 'timer'
+                'appServices', 'appControllers', 'appDirectives', 'ui.router', 'ui.router.state.events', 'ngDialog', 'restangular', 'jm.i18next', 'timer', 'ui.bootstrap'
                 ])
             .run(['$rootScope', '$state', 'User', '$stateParams', '$http', '$filter', function ($rootScope, $state, User, $stateParams, $http, $filter) {
                         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded;charset=utf-8";
